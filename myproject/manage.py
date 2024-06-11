f7bce0f3950b374ab6b85e67c29c4c50a0397c62
luka akhaladze <luka.akhaladze@makingscience.com>
2024-06-11 12:54:24 +0400

    testing build trigger

--- myproject/manage.py
+++ myproject/manage.py
@@ -16,7 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    print('suup')
+    print('testing buildtrigger')
 
 
 if __name__ == '__main__':
