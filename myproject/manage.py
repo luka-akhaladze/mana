569137ef9859e545b34860953fef2aceb7d81e11
luka akhaladze <luka.akhaladze@makingscience.com>
2024-06-11 13:22:55 +0400

    testing webhook

--- myproject/manage.py
+++ myproject/manage.py
@@ -16,7 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    print('testing buildtrigger')
+    print('testing buildtrigger 1.0')
 
 
 if __name__ == '__main__':
